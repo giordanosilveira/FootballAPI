b268aeb8905f31cded29fbc45f3eb79ad848c670
Giordano Silveira <giordanovraau0204@gmail.com>
2026-05-13 22:27:23 -0300

    deleting create method from IBaseRepository

--- backend/src/domain/repositories/IBaseRepository.js
+++ backend/src/domain/repositories/IBaseRepository.js
@@ -2,7 +2,6 @@ class IBaseRepository {
     async findAll() {throw new Error('Method not implemented');}
     async findById(id) {throw new Error('Method not implemented');}
     async findByField(field, value) {throw new Error('Method not implemented');}
-    async create(entity) {throw new Error('Method not implemented');}
     async update(id, entity) {throw new Error('Method not implemented');}
     async save(entity) {throw new Error('Method not implemented');}
     async delete(id) {throw new Error('Method not implemented');}
