1dd87cd2b0fbf36e9e1fac0b561ad32b80d29dc2
Giordano Silveira <giordanovraau0204@gmail.com>
2026-05-20 23:23:51 -0300

    Add GetCountryById use case

---
+++ backend/src/application/use-cases/country/GetCountryById.js
@@ -0,0 +1,25 @@
+const ValidationError = require('../../../domain/errors/ValidationError');
+const NotFoundError = require('../../../domain/errors/NotFoundError');
+
+class GetCountryById {
+    constructor(countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    async execute(id) {
+        if (!id) {
+            throw new ValidationError('ID is required');
+        }
+
+        // Validate that the ID is integer format
+        if (!Number.isInteger(id)) {
+            throw new ValidationError('ID must be an integer');
+        }
+
+        const country = await this.countryRepository.findById(id);
+        if (!country) {
+            throw new NotFoundError('Country', id);
+        }
+        return country;
+    }
+}
