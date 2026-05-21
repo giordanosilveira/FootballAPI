b7233ebf8fe125830ee7530580a724bbf216e4fa
Giordano Silveira <giordanovraau0204@gmail.com>
2026-05-20 23:23:28 -0300

    Add GetAllCountries use case

---
+++ backend/src/application/use-cases/country/GetAllCountries.js
@@ -0,0 +1,11 @@
+class GetAllCountries {
+    constructor(countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    async execute() {
+        return await this.countryRepository.findAll();
+    }
+}
+
+module.exports = GetAllCountries;
