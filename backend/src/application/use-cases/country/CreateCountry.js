6bc934ee187a3759676fea9326d86f88324a54ed
Giordano Silveira <giordanovraau0204@gmail.com>
2026-05-20 23:22:49 -0300

    add CreateCountry use case

---
+++ backend/src/application/use-cases/country/CreateCountry.js
@@ -0,0 +1,24 @@
+const ConflictError = require('../../../domain/errors/ConflictError');
+const ValidationError = require('../../../domain/errors/ValidationError');
+
+class CreateCountry {
+  constructor(countryRepository) {
+    this.countryRepository = countryRepository;
+  }
+
+  async execute({ name, code, flag }) {
+    if (!name || !code) {
+      throw new ValidationError('Name and code are required');
+    }
+    
+    const existingCountry = await this.countryRepository.findByCode(code);
+    if (existingCountry) {
+      throw new ConflictError(`Country with code '${code}' already exists`);
+    }
+    
+    const country = await this.countryRepository.create({ name, code, flag });
+    return country;
+  }
+}
+
+module.exports = CreateCountry;
